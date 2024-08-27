f8a63be573a0146d76aaf8d27aa9360dbbc2f8da
nikitabelous <nikitabelous16@gmail.com>
2024-08-27 17:25:17 +0300

    add const to amount tokens

--- src/api/modules/faucet/helpers/index.ts
+++ src/api/modules/faucet/helpers/index.ts
@@ -1,5 +1,7 @@
 import { api } from '@/api/clients'
 
+const DEFAULT_AMOUNT_TOKENS_FOR_NEW_USERS = 1
+
 export const getToken = async (address: string, amount?: string): Promise<void> => {
   await api.post('/integrations/faucet', {
     body: {
@@ -8,7 +10,7 @@ export const getToken = async (address: string, amount?: string): Promise<void> 
         attributes: {
           recipient: {
             address: address,
-            amount: amount ?? 1,
+            amount: amount ?? DEFAULT_AMOUNT_TOKENS_FOR_NEW_USERS,
           },
         },
       },
