b37f29902dd97241b86c09cc1182ff6b144a24d5
nikitabelous <nikitabelous16@gmail.com>
2024-09-12 13:24:19 +0300

    remove mock data and fetch roles from graph

--- src/pages/Roles/components/RoleCard.tsx
+++ src/pages/Roles/components/RoleCard.tsx
@@ -1,12 +1,18 @@
-import { Chip, Stack, Typography, useTheme } from '@mui/material'
+import { Chip, Stack, Typography } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 
 import { FontWeight } from '@/theme/constants'
 
+type resource = {
+  id: string
+  name: string
+  allows: string[]
+  disallows: string[]
+}
+
 type Props = {
   title: string
-  desc: string
-  permissions: string[]
+  resources: resource[]
 }
 
 const style = {
@@ -15,27 +21,43 @@ const style = {
   direction: 'column',
   border: '1px solid',
   borderColor: 'secondary.light',
-  height: 250,
+  minHeight: 250,
 }
 
-const SuccessKYCModal = ({ title, desc, permissions }: Props) => {
-  const { palette } = useTheme()
+const SuccessKYCModal = ({ title, resources }: Props) => {
   const { t } = useTranslation()
 
   return (
     <Stack sx={style}>
-      <Typography variant='h5' fontWeight={FontWeight.SemiBold}>
+      <Typography variant='h6' fontWeight={FontWeight.SemiBold}>
         {title}
       </Typography>
-      <Typography sx={{ fontSize: 14, color: palette.primary.light }} mt={3}>
-        {desc}
-      </Typography>
       <Typography variant='body3' mt={4}>
-        {t('role-card.permissions')}
+        {t('role-card.resources')}
       </Typography>
       <Stack direction='column' spacing={2} mt={4} alignItems='flex-start'>
-        {permissions.map((permission, idx) => (
-          <Chip key={idx} size='medium' label={permission} />
+        {resources.map((resource, idx) => (
+          <Stack key={idx}>
+            <Typography variant='body3'>
+              {resource.name === '*' ? 'All resources' : resource.name}
+            </Typography>
+            <Stack direction='row' gap={2} alignItems='center'>
+              <Typography variant='caption1'>Allow:</Typography>
+              {resource.allows.map((label, index) => (
+                <Chip key={index} size='small' label={label === '*' ? 'All' : label} />
+              ))}
+            </Stack>
+            {Boolean(resource.disallows.length) && (
+              <Stack direction='row' gap={2}>
+                <Typography variant='caption1' alignItems='center'>
+                  Disallows:
+                </Typography>
+                {resource.disallows.map((label, index) => (
+                  <Chip key={index} size='small' label={label === '*' ? 'All' : label} />
+                ))}
+              </Stack>
+            )}
+          </Stack>
         ))}
       </Stack>
     </Stack>
