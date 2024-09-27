f5d108a1f32e2f6cb672557c58f91748b2fcb273
nikitabelous <nikitabelous16@gmail.com>
2024-09-27 13:43:12 +0300

    remove comment

--- src/api/modules/keyserver/helpers/strkey.ts
+++ src/api/modules/keyserver/helpers/strkey.ts
@@ -1,6 +1,5 @@
 import * as crypto from 'crypto'
 
-// Функция для декодирования публичного или приватного ключа из hex-строки
 export function decodeHex(encoded: string): Buffer {
   if (!encoded.startsWith('0x')) {
     throw new Error('Invalid format: missing "0x" prefix.')
@@ -8,8 +7,6 @@ export function decodeHex(encoded: string): Buffer {
 
   return Buffer.from(encoded.slice(2), 'hex')
 }
-
-// Функция для кодирования данных (например, ключей) в hex-строку с версией байта
 
 export function encodeHex(data: Buffer): string {
   if (!data) {
@@ -19,7 +16,6 @@ export function encodeHex(data: Buffer): string {
   return '0x' + data.toString('hex')
 }
 
-// Проверка контрольной суммы для Ethereum-адреса (EIP-55)
 export function isValidChecksumAddress(address: string): boolean {
   // Убедимся, что адрес имеет длину 42 символа (0x + 40 hex символов)
   if (address.length !== 42 || !/^0x[0-9a-fA-F]{40}$/.test(address)) {
@@ -31,7 +27,6 @@ export function isValidChecksumAddress(address: string): boolean {
     .update(address.slice(2).toLowerCase())
     .digest('hex')
 
-  // Проверяем каждую букву в адресе по правилу EIP-55
   for (let i = 0; i < 40; i++) {
     const char = address[i + 2]
     const hashChar = parseInt(addressHash[i], 16)
