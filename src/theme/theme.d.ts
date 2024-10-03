45adadf564788aff3f12207d1e461c1435d2f527
nikitabelous <nikitabelous16@gmail.com>
2024-10-03 13:40:42 +0300

    add colors and typography

---
+++ src/theme/theme.d.ts
@@ -0,0 +1,57 @@
+import '@mui/material/styles'
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    icon: {
+      primaryLight: string
+      primary: string
+      primaryDark: string
+    }
+    border: {
+      light: string
+      main: string
+      dark: string
+    }
+    disable: {
+      light: string
+      main: string
+      dark: string
+    }
+  }
+
+  interface PaletteOptions {
+    icon?: {
+      primaryLight?: string
+      primary?: string
+      primaryDark?: string
+    }
+    border?: {
+      light?: string
+      main?: string
+      dark?: string
+    }
+    disable?: {
+      light?: string
+      main?: string
+      dark?: string
+    }
+  }
+
+  interface TypeText {
+    primaryLight: string
+    primaryDark: string
+    secondaryLight: string
+    secondaryDark: string
+    accent: string
+  }
+
+  interface TypeBackground {
+    primaryLight: string
+    primary: string
+    primaryDark: string
+    secondaryLight: string
+    secondary: string
+    secondaryDark: string
+    tertiary: string
+  }
+}
